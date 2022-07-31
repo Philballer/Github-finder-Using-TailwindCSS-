faeab1b1108f10902e595635988f73dcc8a747c8
Philballer <philchukwueze@outlook.com>
2022-07-31 12:02:05 +0200

    introduces Reducer to manage all states

--- src/Context/Github/GithubContext.js
+++ src/Context/Github/GithubContext.js
@@ -1,25 +1,37 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import GithubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const ACTION = {
+    GET_USERS: 'get_users',
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`);
     const data = await response.json();
-    setUsers(data);
-    setLoading(false);
+    dispatch({
+      type: ACTION.GET_USERS,
+      payload: data,
+    });
+    // reason why we dont have two dispatches is cause action.GET_USERS activates the two states in the reducer
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
--- src/Context/Github/GithubReducer.js
+++ src/Context/Github/GithubReducer.js
@@ -1,5 +1,12 @@
 const GithubReducer = (state, action) => {
   switch (action.type) {
+    case 'get_users': {
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    }
     default:
       return state;
     //if nothing has happened initial state should be returned
